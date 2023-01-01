9980e8b8fcfeade549874bbc2f8f9da7761d3ae8
Harsh Prajapati <golaharsh2001@gmail.com>
2023-01-01 19:22:28 +0530

    updated socketio ENDPOINT to url of render

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -22,7 +22,7 @@ import Lottie from "react-lottie";
 import animationData from "../animation/typing.json";
 import SendIcon from "@mui/icons-material/Send";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://talkative.onrender.com";
 
 let socket, selectedChatCompare;
 
