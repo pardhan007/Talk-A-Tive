8a4b52f1ebc25067639c81389ebf65d067882949
Harsh Prajapati <harsh@Harshs-MacBook-Air.local>
2023-03-08 01:36:12 +0530

    changed proxy to server url

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        '/api',
+        "/api",
         createProxyMiddleware({
-            target: 'http://localhost:5100',
+            target: "https://talkative.onrender.com",
             changeOrigin: true,
         })
     );
